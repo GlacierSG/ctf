8637fd5bd09be3bd1d36f437cc214b858f5e4046
GlacierSG <iamglaciersg@gmail.com>
2025-11-27 10:14:03 +0000

    small stuff

---
+++ src/ctf/enumeration.py
@@ -0,0 +1,21 @@
+from .template_util import *
+from dataclasses import dataclass
+
+
+@dataclass(slots=True)
+class Enumerate():
+    ip: str
+    
+    def __post_init__(self):
+        if not self.ip:
+            raise ValueError("Enumerate needs to have an ip")
+    def _run_comand(cmd):
+        out = subprocess.run(cmd, capture_output=True)
+        if out.returncode == 0:
+            return out.stdout
+        else:
+            print(f"\033[1;31m[STDERR]\033[0m {''.join(cmd)} resulted in {out.stderr}", file=sys.stderr)
+
+
+Enumerate.nmap_fast = lambda self: \
+    self._run_command(['nmap', self.ip]).split('\n')
--- src/ctf/template_util.py
+++ src/ctf/template_util.py
@@ -13,15 +13,13 @@ def isinstalled(modules):
             found = False
     return found
 
-def _python_dir():
-    return (getattr(sys, "base_prefix", None) or getattr(sys, "real_prefix", None) or sys.prefix)
 def install(modules):
     if isinstance(modules, str):
         modules = [modules]
 
     for module in modules:
         if not isinstalled(module):
-            subprocess.check_call([f"{_python_dir()}/bin/python", "-m", "pip", "install", module])
+            subprocess.check_call([f"{sys.prefix}/bin/python", "-m", "pip", "install", module])
 
 from ast import literal_eval 
 import hashlib
